3e369dfe15b8a3faf16a751791781575bae402f7
Patrick Rodrigues <rodpadev@rodriguespatrick.com>
2020-03-30 07:24:10 +0200

    change import name

--- app/index.js
+++ app/index.js
@@ -1,15 +1,14 @@
 import React, { Fragment } from 'react';
 import { render } from 'react-dom';
 import { AppContainer as ReactHotAppContainer } from 'react-hot-loader';
-import Root from './components/App';
-
+import App from './components/App';
 import './app.global.css';
 
 const AppContainer = process.env.PLAIN_HMR ? Fragment : ReactHotAppContainer;
 
 render(
   <AppContainer>
-    <Root />
+    <App />
   </AppContainer>,
   document.getElementById('root')
 );
