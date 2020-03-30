5e874e8013870419207d1de45d1d6b0d698423c7
Patrick Rodrigues <rodpadev@rodriguespatrick.com>
2020-03-30 09:16:57 +0200

    create & import counter

--- app/components/root.jsx
+++ app/components/root.jsx
@@ -1,17 +1,23 @@
-import { hot } from 'react-hot-loader/root';
-import React from 'react';
-import bones from './bones.svg'
-import styles from './root.css'
+import { hot } from "react-hot-loader/root";
+import React from "react";
+import bones from "./bones.svg";
+import styles from "./root.css";
+import Counter from "../components/counter";
 
+function Root() {
 
-
-const Root = () => (
-  <>
+  return(
+    <>
     <div className={styles.main_div}>
+
       <h1>Electron React Barebones</h1>
-      <img src={bones} atl="" id={styles.bones}/>
+
+      <img src={bones} atl="" id={styles.bones} />
+      
+      <Counter/>
     </div>
   </>
-);
+  )
+};
 
 export default hot(Root);
